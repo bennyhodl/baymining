3db6213150f9e4df9e1a8e7c3cb9206a0aa191e9
bennyhodl <ben@blockspaces.com>
2022-06-16 21:58:32 -0400

    Payout page

--- src/views/admin/dataTables/index.jsx
+++ src/views/admin/dataTables/index.jsx
@@ -21,43 +21,114 @@
 */
 
 // Chakra imports
-import { Box, SimpleGrid } from "@chakra-ui/react";
+import { Box, Flex, FormLabel, Avatar, Select, SimpleGrid, Icon, useColorModeValue } from "@chakra-ui/react";
+
+// Header box components
+import MiniStatistics from "components/card/MiniStatistics";
+import IconBox from "components/icons/IconBox";
+import { MdAddTask, MdAttachMoney, MdBarChart, MdFileCopy } from "react-icons/md";
+import Usa from "assets/img/dashboards/usa.png";
+
+// Main content
 import DevelopmentTable from "views/admin/dataTables/components/DevelopmentTable";
-import CheckTable from "views/admin/dataTables/components/CheckTable";
-import ColumnsTable from "views/admin/dataTables/components/ColumnsTable";
-import ComplexTable from "views/admin/dataTables/components/ComplexTable";
-import {
-  columnsDataDevelopment,
-  columnsDataCheck,
-  columnsDataColumns,
-  columnsDataComplex,
-} from "views/admin/dataTables/variables/columnsData";
+import { columnsDataDevelopment } from "views/admin/dataTables/variables/columnsData";
 import tableDataDevelopment from "views/admin/dataTables/variables/tableDataDevelopment.json";
-import tableDataCheck from "views/admin/dataTables/variables/tableDataCheck.json";
-import tableDataColumns from "views/admin/dataTables/variables/tableDataColumns.json";
-import tableDataComplex from "views/admin/dataTables/variables/tableDataComplex.json";
 import React from "react";
 
 export default function Settings() {
   // Chakra Color Mode
+  const brandColor = useColorModeValue("brand.500", "white");
+  const boxBg = useColorModeValue("secondaryGray.300", "whiteAlpha.100");
   return (
     <Box pt={{ base: "130px", md: "80px", xl: "80px" }}>
       <SimpleGrid
+        columns={{ base: 1, md: 2, lg: 3, "2xl": 6 }}
+        gap='20px'
+        mb='20px'>
+        <MiniStatistics
+          startContent={
+            <IconBox
+              w='56px'
+              h='56px'
+              bg={boxBg}
+              icon={
+                <Icon w='32px' h='32px' as={MdBarChart} color={brandColor} />
+              }
+            />
+          }
+          name='Earnings'
+          value='$350.4'
+        />
+        <MiniStatistics
+          startContent={
+            <IconBox
+              w='56px'
+              h='56px'
+              bg={boxBg}
+              icon={
+                <Icon w='32px' h='32px' as={MdAttachMoney} color={brandColor} />
+              }
+            />
+          }
+          name='Spend this month'
+          value='$642.39'
+        />
+        <MiniStatistics growth='+23%' name='Sales' value='$574.34' />
+        <MiniStatistics
+          endContent={
+            <Flex me='-16px' mt='10px'>
+              <FormLabel htmlFor='balance'>
+                <Avatar src={Usa} />
+              </FormLabel>
+              <Select
+                id='balance'
+                variant='mini'
+                mt='5px'
+                me='0px'
+                defaultValue='usd'>
+                <option value='usd'>USD</option>
+                <option value='eur'>EUR</option>
+                <option value='gba'>GBA</option>
+              </Select>
+            </Flex>
+          }
+          name='Your balance'
+          value='$1,000'
+        />
+        <MiniStatistics
+          startContent={
+            <IconBox
+              w='56px'
+              h='56px'
+              bg='linear-gradient(90deg, #4481EB 0%, #04BEFE 100%)'
+              icon={<Icon w='28px' h='28px' as={MdAddTask} color='white' />}
+            />
+          }
+          name='New Tasks'
+          value='154'
+        />
+        <MiniStatistics
+          startContent={
+            <IconBox
+              w='56px'
+              h='56px'
+              bg={boxBg}
+              icon={
+                <Icon w='32px' h='32px' as={MdFileCopy} color={brandColor} />
+              }
+            />
+          }
+          name='Total Projects'
+          value='2935'
+        />
+      </SimpleGrid>
+      <SimpleGrid
         mb='20px'
-        columns={{ sm: 1, md: 2 }}
+        columns={{ sm: 1, md: 1 }}
         spacing={{ base: "20px", xl: "20px" }}>
         <DevelopmentTable
           columnsData={columnsDataDevelopment}
           tableData={tableDataDevelopment}
-        />
-        <CheckTable columnsData={columnsDataCheck} tableData={tableDataCheck} />
-        <ColumnsTable
-          columnsData={columnsDataColumns}
-          tableData={tableDataColumns}
-        />
-        <ComplexTable
-          columnsData={columnsDataComplex}
-          tableData={tableDataComplex}
         />
       </SimpleGrid>
     </Box>
